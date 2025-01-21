b12a043efb7aa629ab058ec396ea86b88b212e1d
kevin083177 <kevin083177@gmail.com>
2025-01-21 16:28:42 +0800

    Add auth and log middleware

--- Backend/routes/user_route.py
+++ Backend/routes/user_route.py
@@ -1,17 +1,22 @@
 from flask import Blueprint
 from controllers.user_controller import UserController
-from middlewares.auth_middleware import token_required
+from middlewares.auth_middleware import *
+from middlewares.log_middleware import log_request
+
 user_blueprint = Blueprint('users', __name__)
 
 @user_blueprint.route('/create_user', methods=['POST'])
+@log_request
 def create_user():
     return UserController.create_user()
 
 @user_blueprint.route('/login', methods=['POST'])
+@log_request
 def login():
     return UserController.login()
 
-@user_blueprint.route('/getUserById/<user_id>', methods=['GET'])
-@token_required
+@user_blueprint.route('/get_own_user/<user_id>', methods=['GET'])
+@log_request
+@self_required
 def get_user(user_id):
     return UserController.get_user(user_id)
