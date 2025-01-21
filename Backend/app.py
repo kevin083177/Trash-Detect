f0e6e3a5fba698302fcfca745b6915563c08f390
kevin083177 <kevin083177@gmail.com>
2025-01-21 16:30:22 +0800

    Add shutdown handler for server

--- Backend/app.py
+++ Backend/app.py
@@ -2,18 +2,53 @@ from flask import Flask
 from flask_cors import CORS
 from config import Config, init_db
 from routes.user_route import user_blueprint
+from utils.logger_config import logger
+from gevent import pywsgi
+import sys, signal
 
 app = Flask(__name__)
 CORS(app)
 
-# Load Flask settings
-app.config.from_object(Config)
+@app.route('/')
+def index():
+    return "Hello"
 
-# Initial MongoDB connection store in app.config
-app.config["MongoDB"] = init_db()
+def signal_handler(sig, frame):
+    logger.info("Server shutting down...")
+    sys.exit(0)
 
-# Blueprint
-app.register_blueprint(user_blueprint, url_prefix="/users")
-    
+def create_app():
+    try:
+        # Load Flask settings
+        app.config.from_object(Config)
+        
+        # Initial MongoDB connection store in app.config
+        mongodb = init_db()
+        app.config["MongoDB"] = mongodb
+        
+        # Log successful MongoDB connection
+        logger.info(f"success: connect to mongoDB @{Config.MONGO_HOST}")
+        
+        # Blueprint
+        app.register_blueprint(user_blueprint, url_prefix="/users")
+        
+        # Log server startup
+        logger.info(f"listening on *:{Config.PORT}")
+        
+        return app
+    except Exception as e:
+        logger.error(f"Failed to start server: {str(e)}")
+        raise e
+
 if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"], host="0.0.0.0", port=Config.PORT)
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    app = create_app()
+    server = pywsgi.WSGIServer(('0.0.0.0', Config.PORT), app, log=None)
+    
+    try:
+        server.serve_forever()
+        
+    except KeyboardInterrupt:
+        logger.info("Server shutting down...")
+        sys.exit(0)
