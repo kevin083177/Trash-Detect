2f5cb8426a05e2e99b115464773b048cacfe8fb9
kevin083177 <kevin083177@gmail.com>
2025-01-26 18:01:18 +0800

    Add api_prefix: "/api/v1"

--- Backend/routes/__init__.py
+++ Backend/routes/__init__.py
@@ -5,8 +5,10 @@ from .auth_route import auth_blueprint
 from .admin_route import admin_blueprint
 from .purchase_route import purchase_blueprint
 
+api_prefix = '/api/v1/'
+
 def register_blueprints(app: Flask):
-    app.register_blueprint(user_blueprint, url_prefix="/users")
-    app.register_blueprint(auth_blueprint, url_prefix="/auth") 
-    app.register_blueprint(admin_blueprint, url_prefix="/admin")
-    app.register_blueprint(purchase_blueprint, url_prefix="/purchase")
+    app.register_blueprint(user_blueprint, url_prefix=f"{api_prefix}users")
+    app.register_blueprint(auth_blueprint, url_prefix=f"{api_prefix}auth") 
+    app.register_blueprint(admin_blueprint, url_prefix=f"{api_prefix}admin")
+    app.register_blueprint(purchase_blueprint, url_prefix=f"{api_prefix}purchase")
