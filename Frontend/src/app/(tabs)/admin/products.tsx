c01c4d3aaf0c2c781dfdf76f8843833b7f716e15
kevin083177 <kevin083177@gmail.com>
2025-02-12 21:41:18 +0800

    Add admin simple screens

---
+++ Frontend/src/app/(tabs)/admin/products.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function AdminProducts() {
+    return (
+        <View>
+            <Text>456</Text>
+        </View>
+    )
+}
---
+++ Frontend/src/app/(tabs)/admin/users.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function AdminUsers() {
+    return (
+        <View>
+            <Text>789</Text>
+        </View>
+    )
+}
