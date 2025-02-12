19fe06952b04e1c86d81f56dc4dd7d7ad43e2997
kevin083177 <kevin083177@gmail.com>
2025-02-12 21:40:16 +0800

    Create _layout.tsx

---
+++ Frontend/src/app/(tabs)/profile/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+
+export default function ProfileLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen 
+        name="settings" 
+        options={{
+            headerShown: true,
+            title: '個人設定',
+            headerBackTitle: '返回'
+        }}
+      />
+    </Stack>
+  );
+}
