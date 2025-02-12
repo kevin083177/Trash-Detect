14ac880bd933a6c93790e56eb42b9813f2ce5c15
kevin083177 <kevin083177@gmail.com>
2025-02-12 21:40:09 +0800

    Create settings.tsx

---
+++ Frontend/src/app/(tabs)/profile/settings.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export default function Setting() {
+    return (
+        <View style={styles.container}>
+            <Text>hi</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    }
+});
