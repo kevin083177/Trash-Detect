17636551e997ad48f8ea9a878cc526577f439d4e
kevin083177 <kevin083177@gmail.com>
2025-04-09 10:21:54 +0800

    refactor: Remove payment_strategy
    - Integrate purchase_product into purchase

--- Backend/controllers/purchase_controller.py
+++ Backend/controllers/purchase_controller.py
@@ -1,7 +1,8 @@
 from flask import request
-from services import PurchaseService, ProductService
+from services import UserService, PurchaseService, ProductService
 from config import Config
 
+user_service = UserService(Config.MONGO_URI)
 purchase_service = PurchaseService(Config.MONGO_URI)
 product_service = ProductService(Config.MONGO_URI)
 
@@ -11,27 +12,29 @@ class PurchaseController:
         try:
             data = request.get_json()
             
-            required_fields = ['product_id', 'payment_type']
-            missing_fields = [field for field in required_fields if field not in data]
-            
-            if missing_fields:
+            if 'product_id' not in data:
                 return {
-                    "message": f"缺少: {', '.join(missing_fields)}",
+                    "message": "缺少 product_id 欄位"  
                 }, 400
             
             product_id = data['product_id']
-            payment_type = data['payment_type']
                                            
-            if purchase_service.check_product_purchased(user['_id'], product_id):
+            if purchase_service._check_product_purchased(user['_id'], product_id):
                 return {
                     "message": f"商品: {product_service.get_product_name(product_id)} 已購買"
                 }, 409
             
+            if user_service._get_user_money(user['_id']) < product_service._get_product_price(product_id):
+                return {
+                    "message": "餘額不足"
+                }, 400
+                
             success, result = purchase_service.purchase_product(
                 user['_id'],
                 product_id,
-                payment_type
             )
+            
+            user_service.subtract_money(user['_id'], product_service._get_product_price(product_id))
             
             if not success:
                 return {"message": result}, 400
