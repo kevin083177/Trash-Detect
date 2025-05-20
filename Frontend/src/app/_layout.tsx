ce4c8cf5474885f65b759c897f87363601b6109a
kevin083177 <kevin083177@gmail.com>
2025-05-20 13:08:21 +0800

    chore: remove ProductProvider

--- Frontend/src/app/_layout.tsx
+++ Frontend/src/app/_layout.tsx
@@ -1,17 +1,14 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '@/hooks/auth';
-import { ProductProvider } from '@/hooks/product';
 import React from 'react';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <ProductProvider>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
         </Stack>
-      </ProductProvider>
     </AuthProvider>
   );
 }
