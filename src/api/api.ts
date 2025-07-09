6c6d23ce7bc3ebf7588ab5f3482cac74e1ed6277
kevin083177 <kevin083177@gmail.com>
2025-07-10 03:44:47 +0800

    feat: Add question_api

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,7 @@ const API_VERSION = '/api/v1';
 const AUTH_URL = `${API_BASE_URL}${API_VERSION}/auth`;
 const THEME_URL = `${API_BASE_URL}${API_VERSION}/theme`;
 const CHAPTER_URL = `${API_BASE_URL}${API_VERSION}/chapter`;
+const QUESTION_URL = `${API_BASE_URL}${API_VERSION}/question`;
 
 export const auth_api = {
     login: `${AUTH_URL}/login`,
@@ -14,9 +15,16 @@ export const theme_api = {
     get_theme: `${THEME_URL}/`,
     add_theme: `${THEME_URL}/add_theme`,
     get_all_themes: `${THEME_URL}/get_all_themes`,
-    get_theme_products: `${THEME_URL}/`, // + theme_name/products
+    get_theme_products: `${THEME_URL}`, // + theme_name/products
 }
 
 export const chapter_api = {
     get_all_chapters: `${CHAPTER_URL}/all`,
+}
+
+export const question_api = {
+    add_question: `${QUESTION_URL}/add_question`,
+    delete_question: `${QUESTION_URL}/delete_question`,
+    update_question: `${QUESTION_URL}/update_question`,
+    get_question_by_category: `${QUESTION_URL}/all` // + chapter name slice(0, -2);
 }
