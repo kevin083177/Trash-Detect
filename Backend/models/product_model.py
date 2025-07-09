ec42283ea52dd8125de8170c25bd42b4faa635ec
kevin083177 <kevin083177@gmail.com>
2025-07-09 15:42:39 +0800

    refactor: Using image model instead own

--- Backend/models/product_model.py
+++ Backend/models/product_model.py
@@ -1,27 +1,7 @@
-class ProductImage:
-    """商品圖片類"""
-    def __init__(self, public_id: str, url: str, thumbnail_url: str):
-        self.public_id = public_id
-        self.url = url
-        self.thumbnail_url = thumbnail_url
-        
-    def to_dict(self):
-        return {
-            "public_id": self.public_id,
-            "url": self.url,
-            "thumbnail_url": self.thumbnail_url
-        }
-        
-    @classmethod
-    def from_dict(cls, data):
-        return cls(
-            public_id=data["public_id"],
-            url=data["url"],
-            thumbnail_url=data["thumbnail_url"]
-        )
+from .image_model import Image
 
 class Product:
-    def __init__(self, name, description, price, theme, type, image=None):
+    def __init__(self, name, description, price, theme, type, image: Image=None):
         self.name = name
         self.description = description
         self.price = price
