bec1acddb5092aca3929093f0b5d2f1a3763cbeb
kevin083177 <kevin083177@gmail.com>
2025-07-11 20:43:56 +0800

    feat: Add get all users info service

--- Backend/services/admin_service.py
+++ Backend/services/admin_service.py
@@ -5,45 +5,39 @@ class AdminService(DatabaseService):
     def __init__(self, mongo_uri):
         super().__init__(mongo_uri)
         self.users = self.collections['users']
-        self.purchase = self.collections['purchases']
-        self.user_levels = self.collections['user_levels']
-        
-    def delete_user(self, user_id):
+    
+    def get_all_users_info(self):
         try:
-            user_id = ObjectId(user_id) if not isinstance(user_id, ObjectId) else user_id
+            pipeline = [
+            {
+                "$lookup": {
+                    "from": "user_levels",
+                    "localField": "_id",
+                    "foreignField": "user_id",
+                    "as": "user_level_data"
+                }
+            },
+            {
+                "$project": {
+                    "_id": {"$toString": "$_id"},
+                    "username": 1,
+                    "trash_stats": 1,
+                    "last_check_in": 1,
+                    "highest_level": {
+                        "$ifNull": [
+                            {"$arrayElemAt": ["$user_level_data.highest_level", 0]},
+                            0
+                        ]
+                    }
+                }
+            },
+        ]
             
-            # 儲存刪除操作結果
-            deletion_results = {
-                'users': False,
-                'purchases': False,
-                'user_levels': False
-            }
+            users_info = list(self.users.aggregate(pipeline))
             
-            user = self.users.find_one_and_delete({"_id": user_id})
-
-            deletion_results['users'] = bool(user)
-            
-            # 刪除 user_purchase
-            purchase = self.purchase.find_one_and_delete({"user_id": user_id})
-            deletion_results['purchases'] = bool(purchase)
-            
-            user_level = self.user_levels.find_one_and_delete({"user_id": user_id})
-            deletion_results['user_levels'] = bool(user_level)
-            
-            # 檢查刪除結果
-            if all(deletion_results.values()):
-                return True, "使用者與相關資料已刪除"
-            
-            failed_deletions = [
-                key for key, value in deletion_results.items() 
-                if not value
-            ]
-            
-            if deletion_results['users'] and failed_deletions:
-                return True, f"使用者已刪除，但以下資料刪除失敗: {', '.join(failed_deletions)}"
-                
-            return False, "無法找到使用者"
+            return users_info
             
         except Exception as e:
-            print(f"Delete User Error: {str(e)}")
+            print(f"Get All Users Info Error: {str(e)}")
             raise
+            
