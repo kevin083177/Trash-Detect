6b288d3ae2c983a8b2447226502c6ca334517fec
kevin083177 <kevin083177@gmail.com>
2025-07-21 20:34:24 +0800

    chore: Remove option in each screen

--- Frontend/src/app/game/_layout.tsx
+++ Frontend/src/app/game/_layout.tsx
@@ -5,8 +5,8 @@ export default function GameLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="gameplay" options={{ headerShown: false }} />
-      <Stack.Screen name="result" options={{ headerShown: false }} />
+      <Stack.Screen name="gameplay" />
+      <Stack.Screen name="result" />
     </Stack>
   );
 }
